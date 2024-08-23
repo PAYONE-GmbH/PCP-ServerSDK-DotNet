3d857498ca1e1d6e53fe4d350c8c49ec30d89002
Blackfaded <r.heinen@nanogiants.de>
2024-08-23 08:26:02 +0200

    chore: add lint rules

--- app/PCPServerSDKDotNetRunner/Program.cs
+++ app/PCPServerSDKDotNetRunner/Program.cs
@@ -1,9 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-using PCPServerSDKDotNet;
+﻿namespace PCPServerSDKDotNetRunner
+{
+    using PCPServerSDKDotNet;
 
-Console.WriteLine("Hello, World!");
-
-
-
-Class1 class1 = new Class1();
-Console.WriteLine(class1.GetClassName());
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.GetClassName());
+        }
+    }
+}
