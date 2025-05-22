8625213cb6d06db03099a0257ecc18fe535b43ba
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2025-05-22 14:55:49 +0200

    feat: add ActionType enum and update MerchantAction to use it

--- lib/PCPServerSDKDotNet/Models/MerchantAction.cs
+++ lib/PCPServerSDKDotNet/Models/MerchantAction.cs
@@ -12,12 +12,11 @@ namespace PCPServerSDKDotNet.Models
     public class MerchantAction
     {
         /// <summary>
-        /// Gets or sets action merchants needs to take in the online payment process. Possible values are:   * REDIRECT - The customer needs to be redirected using the details found in redirectData   * SHOW_FORM - The customer needs to be shown a form with the fields found in formFields. You can submit the data entered by the user in a Complete payment request.   * SHOW_INSTRUCTIONS - The customer needs to be shown payment instruction using the details found in showData. Alternatively the instructions can be rendered by us using the instructionsRenderingData   * SHOW_TRANSACTION_RESULTS - The customer needs to be shown the transaction results using the details found in showData. Alternatively the instructions can be rendered by us using the instructionsRenderingData   * MOBILE_THREEDS_CHALLENGE - The customer needs to complete a challenge as part of the 3D Secure authentication inside your mobile app. The details contained in mobileThreeDSecureChallengeParameters need to be provided to the EMVco certified Mobile SDK as a challengeParameters object.   * CALL_THIRD_PARTY - The merchant needs to call a third party using the data found in thirdPartyData.
+        /// Gets or sets action merchants needs to take in the online payment process.
         /// </summary>
-        /// <value>Action merchants needs to take in the online payment process. Possible values are:   * REDIRECT - The customer needs to be redirected using the details found in redirectData   * SHOW_FORM - The customer needs to be shown a form with the fields found in formFields. You can submit the data entered by the user in a Complete payment request.   * SHOW_INSTRUCTIONS - The customer needs to be shown payment instruction using the details found in showData. Alternatively the instructions can be rendered by us using the instructionsRenderingData   * SHOW_TRANSACTION_RESULTS - The customer needs to be shown the transaction results using the details found in showData. Alternatively the instructions can be rendered by us using the instructionsRenderingData   * MOBILE_THREEDS_CHALLENGE - The customer needs to complete a challenge as part of the 3D Secure authentication inside your mobile app. The details contained in mobileThreeDSecureChallengeParameters need to be provided to the EMVco certified Mobile SDK as a challengeParameters object.   * CALL_THIRD_PARTY - The merchant needs to call a third party using the data found in thirdPartyData.</value>
         [DataMember(Name = "actionType", EmitDefaultValue = false)]
         [JsonProperty(PropertyName = "actionType")]
-        public string? ActionType { get; set; }
+        public ActionType? ActionType { get; set; }
 
         /// <summary>
         /// Gets or Sets RedirectData.
