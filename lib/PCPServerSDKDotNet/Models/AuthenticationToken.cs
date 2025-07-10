21ed4c10165d5d92102c0eecbc5eb3016dc47191
Blackfaded <r.heinen@nanogiants.de>
2025-07-10 15:57:57 +0200

    chore: update models

--- lib/PCPServerSDKDotNet/Models/AuthenticationToken.cs
+++ lib/PCPServerSDKDotNet/Models/AuthenticationToken.cs
@@ -1,20 +1,51 @@
 namespace PCPServerSDKDotNet.Models
 {
     using System;
-    using System.Text.Json.Serialization;
+    using System.Runtime.Serialization;
+    using Newtonsoft.Json;
 
+    /// <summary>
+    /// Model for authentication JWT token response.
+    /// </summary>
+    [DataContract]
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class AuthenticationToken
     {
-        [JsonPropertyName("id")]
+        /// <summary>
+        /// Gets or sets the token id.
+        /// </summary>
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; } = string.Empty;
 
-        [JsonPropertyName("token")]
+        /// <summary>
+        /// Gets or sets the JWT token string.
+        /// </summary>
+        [DataMember(Name = "token", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "token")]
         public string Token { get; set; } = string.Empty;
 
-        [JsonPropertyName("creationDate")]
+        /// <summary>
+        /// Gets or sets the creation date.
+        /// </summary>
+        [DataMember(Name = "creationDate", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "creationDate")]
         public DateTime CreationDate { get; set; }
 
-        [JsonPropertyName("expirationDate")]
+        /// <summary>
+        /// Gets or sets the expiration date.
+        /// </summary>
+        [DataMember(Name = "expirationDate", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "expirationDate")]
         public DateTime ExpirationDate { get; set; }
+
+        /// <summary>
+        /// Get the string presentation of the object.
+        /// </summary>
+        /// <returns>String presentation of the object.</returns>
+        public override string ToString()
+        {
+            return $"AuthenticationToken {{ Id = {this.Id}, Token = {this.Token}, CreationDate = {this.CreationDate}, ExpirationDate = {this.ExpirationDate} }}";
+        }
     }
 }
